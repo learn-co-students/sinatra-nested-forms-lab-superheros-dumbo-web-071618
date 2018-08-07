41f7d9eea3705c288385d5424a30b512a4e7b4b8
github username <github email address>
2018-08-07 17:04:59 -0400

    Adds solutions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,17 @@ require 'sinatra/base'
 class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
+    
+    get '/' do
+        status 200
+        erb :super_hero
+    end
 
+    post '/team' do
+        @team = params[:team]
+        @members = @team[:members].map { |member| Hero.new(member) }
+        status 200
+        erb :team
+    end
 
 end
